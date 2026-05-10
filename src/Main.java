910ebdcfb18e7a26ccc2d4a4b6b53866c81ba565
Thiago Oliveira <thiagodevoliveira@hotmail.com>
2026-05-10 00:44:42 -0300

    add README

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
+        Mentoria mentoria1 = new Mentoria();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        curso1.setTitulo("Curso Java QA");
+        curso1.setDescricao("Desenvolvimento Java para QA");
+        curso1.setCargaHoraria(7);
+
+        System.out.println(curso1);
+
     }
 }
