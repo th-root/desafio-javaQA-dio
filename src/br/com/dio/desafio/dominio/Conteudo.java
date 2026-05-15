e8a5a1fb4e3515d3d6c6b4ced26f210171b36b11
Thiago Oliveira <thiagodevoliveira@hotmail.com>
2026-05-15 20:06:06 -0300

    Herança e polimorfismo

---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,34 @@
+package br.com.dio.desafio.dominio;
+
+public abstract class Conteudo {
+
+    protected final static double XP_PADRAO = 10D;
+    private String titulo;
+    private String descricao;
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Conteudo{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
